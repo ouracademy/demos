050d28923bb34f9e51f41e86c7d34ad4ba169390
diana <qpdiam@gmail.com>
2021-02-01 21:20:27 -0500

    feat: add image in pdf

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, send_file
+from pdf import addImages 
+from util import getPath, storageFiles
+app = Flask(__name__,  static_url_path="/files")
+
+@app.route('/pdf', methods=['POST'])
+def imgInPdf():
+    file = request.files['input']
+    vb = request.files['vb']
+    sign = request.files['sign']
+    f1, f2, f3 = storageFiles(file, vb, sign)
+    output_file = addImages(f1, f2, f3, file.name)
+    return { "file": output_file }
+
+
+@app.route('/pdf', methods=['GET'])
+def getPdf():
+    data = request.args
+    output_file = getPath(data.get('name'))
+    return send_file(output_file)
+
+if __name__ == '__main__':
+    app.run()
---
+++ pdf.py
@@ -0,0 +1,22 @@
+import fitz
+from util import getPath
+
+def shape_sign(x,y):
+    w, h = (150,100) 
+    return x,y, x+w,y+h
+
+
+def addImages(origin, vb, sign, output):
+    image_rectangle = fitz.Rect(500,40,550,100)
+    file_handle = fitz.open(origin)
+    num_pages = file_handle.pageCount
+    for page_number in range(0,num_pages):
+        page = file_handle[page_number]
+        page.insertImage(image_rectangle, filename=vb)
+        if page_number == num_pages - 1:
+            x,y,x1,y1 = page.searchFor("Lima, 04 de enero de 2021")[0]
+            rectangule = fitz.Rect(shape_sign(x+30,y+30))
+            page.insertImage(rectangule, filename=sign)
+    output_file = output+'-out.pdf'
+    file_handle.save(getPath(output_file))
+    return output_file
